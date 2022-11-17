6cdecb33618358db55e58147989e4a8e912267b8
SerGlush <Shoka583@gmail.com>
2022-11-17 22:24:26 +0300

    Serega changes

--- app/src/main/java/by/bondarik/guessthenumber/ui/settings/SettingsFragment.java
+++ app/src/main/java/by/bondarik/guessthenumber/ui/settings/SettingsFragment.java
@@ -2,11 +2,18 @@ package by.bondarik.guessthenumber.ui.settings;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import by.bondarik.guessthenumber.R;
 
@@ -19,6 +26,9 @@ public class SettingsFragment extends Fragment {
 
     public SettingsFragment() {}
 
+    private EditText editSetting;
+
+    private Button okButton;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -38,5 +48,19 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editSetting = view.findViewById(R.id.edit_num);
+        okButton = view.findViewById(R.id.ok_button);
+
+        editSetting.setOnClickListener(v -> editSetting.selectAll());
+        okButton.setOnClickListener(v -> {
+            TextView textView = requireActivity().findViewById(R.id.textView);
+            textView.setText(editSetting.getText());
+        });
     }
 }
